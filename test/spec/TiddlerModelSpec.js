f3b521782705e38e43436618d6d7b03d60a7a13c
Ben Paddock <pads@thisispads.me.uk>
2012-09-25 19:45:46 +0100

    Test for more default attributes, bag URL and ID attribute.

--- test/spec/TiddlerModelSpec.js
+++ test/spec/TiddlerModelSpec.js
@@ -1,6 +1,8 @@
 describe('Tiddler Model Tests', function () {
 
-    it('should be created with the correct default attributes set', function () {
+    var TiddlerModel;
+
+    beforeEach(function () {
         var mockStatus = {
             attributes: {
                 space: {
@@ -8,10 +10,31 @@ describe('Tiddler Model Tests', function () {
                 }
             }
         };
-        var TiddlerModel = testr('tiddlerModel', {'backbone': Backbone, 'status': mockStatus });
+        TiddlerModel = testr('tiddlerModel', {'backbone': Backbone, 'status': mockStatus });
+    });
+
+    it('should be created with the correct default attributes set', function () {
 
         var tiddlerInstance = new TiddlerModel();
+
         expect(tiddlerInstance.get('title')).toEqual('New Tiddler');
+        expect(tiddlerInstance.get('text')).toEqual('');
+        expect(tiddlerInstance.get('tags')).toEqual([]);
+        expect(tiddlerInstance.get('fields')).toEqual({});
+    });
+
+    it('should point to the correct bag URL', function () {
+
+        var tiddlerInstance = new TiddlerModel();
+
+        expect(tiddlerInstance.urlRoot).toEqual('/bags/test_public/tiddlers');
+    });
+
+    it('should use the title attribute as the unique identifier', function () {
+
+        var tiddlerInstance = new TiddlerModel();
+
+        expect(tiddlerInstance.idAttribute).toEqual('title');
     });
 
 });
