78cac7722008f77adf0c15489a73e1c4c85a1aad
Ben Paddock <pads@thisispads.me.uk>
2012-09-25 22:52:09 +0100

    Add lint and watch config.

--- grunt.js
+++ grunt.js
@@ -2,6 +2,15 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
+        lint: {
+            files: ['assets/T*.js', 'assets/UserStatus.js', 'test/spec/*.js']
+        },
+
+        watch: {
+            files: ['assets/*js', 'test/spec/*,js'],
+            tasks: 'jasmine'
+        },
+
         jasmine: {
             src : 'assets/require.js',
             helpers : 'test/lib/*.js',
@@ -11,4 +20,5 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-jasmine-runner');
 
+    grunt.registerTask('default', 'lint jasmine');
 };
