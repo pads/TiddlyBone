2fd77852394721ba14c1232144e98e5bbafa2bbf
Ben Paddock <pads@thisispads.me.uk>
2012-09-27 12:45:21 +0100

    Managed by Jam.

--- assets/hbt.js
+++ assets/hbt.js
@@ -1,18 +1,1 @@
-define(function(require) {
-
-    var Handlebars = require('handlebars');
-
-    return {
-        load: function(name, require, callback, config) {
-            config || (config = {});
-            config.hbt || (config.hbt = {});
-            config.hbt.extension || (config.hbt.extension = 'handlebars');
-
-            require(['text!' + name + '.' + config.hbt.extension], function(content) {
-                var template = Handlebars.compile(content);
-                callback(template);
-            });
-        }
-    };
-
-});
+../jam/hbt/hbt.js
